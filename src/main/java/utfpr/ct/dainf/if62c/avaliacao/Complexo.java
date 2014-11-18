8b9f8b900d432102d1399b3329cc1c009fa24a9f
ithiro <ithiro_sumi@hotmail.com>
2014-11-18 11:34:39 -0200

    Update Complexo.java

--- src/main/java/utfpr/ct/dainf/if62c/avaliacao/Complexo.java
+++ src/main/java/utfpr/ct/dainf/if62c/avaliacao/Complexo.java
@@ -15,31 +15,57 @@ public class Complexo {
     public Complexo() {
     }
 
-    public Complexo(double real, double img) {
-        // completar a implementação
+    public Complexo(double a, double b) {
+        public double real = a;
+        public double img = b;
     }
 
-    // implementar getReal()
+    public getReal() {
+        return real;
+    }
 
-    // implementar getImg()
+    public getImg() {
+        return img;
+    }
 
     public Complexo soma(Complexo c) {
         return new Complexo(real + c.real, img + c.img);
     }
     
-    // implementar sub(Complexo)
+    public Complexo sub(Complexo d) {
+        return new Complexo(real - d.real, img - d.img);
+    }
 
-    // implementar prod(double)
+    public Complexo prod(double e) {
+        return new Complexo(real * e, img * e);
+    }
 
-    // implementar prod(Complexo)
+    public Complexo prod(Complexo f) {
+        return new Complexo(real * f.real - img * f.img, img * f.real + real * f.img);
+    }
     
-    // implementar div(Complexo)
+    public Complexo div(Complexo g) {
+        return new Complexo((real * g.real + img * g.img)/(g.real * g.real + g.img * g.img),
+        (img * g.real - real * g.img)/(g.real * g.real + g.img * g.img));
+    }
     
-    // implementar sqrt()
     public Complexo[] sqrt() {
-        // completar implementação
-        // retornar o vetor contendo as raízes
-        return null;
+        p = Math.sqrt(real * real + img * img);
+        if (real > 0)      {i = Math.atan(img/real);}
+        else if (real < 0) {i = Math.atan(img/real) + Math.PI;}
+        else {
+            if (img == 0) {i = 0;}
+            else if (img > 0) {i = Math.PI/2;}
+            else {i = 3*Math.PI/2}
+        }
+        oi = i/2;
+        oii = i/2 + Math.PI;
+        Complexo[] r = new Complexo[2];
+        r[0].real = p * Math.cos(oi)
+        r[0].img = p * Math.sin(oi)
+        r[1].real = p * Math.cos(oii)
+        r[1].img = p * Math.sin(oii)
+        return r;
     }
 
     @Override
